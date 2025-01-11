d7dc029358f60152169a639f9e080992d3685fd5
Vivek Tripathi <vivektrip18@gmail.com>
2025-01-11 19:41:57 +0530

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Button, Text, View } from "react-native";
+
+export default function () {
+  return <View>
+      <Text>hello there</Text>
+      <Button title="Signup"></Button>
+    </View>
+  
+}
